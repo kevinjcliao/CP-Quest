eb6dbc9fbeefd68660ec7830c46178c65f2c2ba5
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-02-01 21:25:25 -0500

    First Chamber created.

---
+++ chambers/chamber_one.py
@@ -0,0 +1,8 @@
+class chamber_one: 
+    def __init__(self, currentGame): 
+        currentGame.slowPrint("You've spent precustoms decorating your dorm. One by one
+        the freshmen start moving in! Exciting! Eager to begin your lives as CPs
+        for Tritton Lower West, you spray paint the halls with...")
+    
+        self.acceptable_inputs = ["Hello"]
+
