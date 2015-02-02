9346835f574c526d5b341c08674c8331a9b01929
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-02-01 21:25:06 -0500

    First chamber created.

--- game.py
+++ game.py
@@ -1,16 +1,22 @@
 import random
 import sys
 import time
+import chambers/chamber_one
 
 class Game: 
     def __init__(self, gamePlayerName): 
         print "New game created! " + "Hi, " + gamePlayerName
     
-    def firstRun (self): 
-        self.slowPrint("You awake in the dark flickering lights of Lunt Basement.")
-
+    def newGame (self): 
+        chamber_one = 
+        
+        self.slowPrint(
     def slowPrint(self, s): 
         for c in s + '\n': 
             sys.stdout.write(c)
             sys.stdout.flush()
             time.sleep(0.01)
+
+    def printAcceptableInputs(self, acceptableInputs): 
+        self.slowPrint("Current acceptable inputs: ")
+        print acceptableInputs
