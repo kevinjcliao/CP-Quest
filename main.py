6e01ab100cd2c04724f13349ffc832065be66702
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-02-01 20:55:36 -0500

    Added user class

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import 
+import users
 
 def main(): 
+    player_name = raw_input("What is your name? ")
+    player = users.User(player_name)
+    print("Hi " + player.user_name+  ", shall we play a game?")
     
 
-if __name__ =="main": 
-    
+
+if __name__ =="__main__":  
+    main()
