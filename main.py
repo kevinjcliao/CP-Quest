eb4758b09862b6d32d0630c3c44af81c95b7e52b
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-02-05 22:43:43 -0500

    Added screen clearing on game launch.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 import users
 import game
 
 def main(): 
+    # Clears screen on Windows computers
+    os.system('cls')
+    
+    # Clears screen on Mac OS X / Linux computers
+    os.system('clear')
     player_name = raw_input("What is your name? ")
     player = users.User(player_name)
     print("Hi " + player.user_name+  ", shall we play a game?")
