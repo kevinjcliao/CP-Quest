d294f834ed2655c363ac1c1f7173588a44d31e1a
Tristan Pepin <tpepin@haverford.edu>
2015-02-06 01:03:27 -0500

    Yooo I set up thre rest of the chambers to simply be filled in. tpepin@haverford.edu

---
+++ chambers/chamber_nine.py
@@ -0,0 +1,43 @@
+import sys
+
+sys.path.append("../")
+
+
+class chamber_three: 
+    def __init__(self, currentGame, currentPlayer): 
+        self.gamePlayer = currentPlayer
+        self.theCurrentGame = currentGame
+        self.acceptable_inputs = []
+        self.printQuestion(self.acceptable_inputs)
+        self.getUserInput(self.acceptable_inputs)
+
+    def printQuestion(self, acceptable_inputs): 
+        self.theCurrentGame.slowPrint("FILL IN Scenario")
+        self.theCurrentGame.slowPrint("!")
+        print acceptable_inputs
+
+    def getUserInput(self, acceptable_inputs): 
+        input = raw_input("Choose an answer: ")
+        print "Great! You chose: " + input
+        
+        for i in range(0,len(self.acceptable_inputs)): 
+
+            if input == self.acceptable_inputs[i]: 
+                if i==0: 
+                    self.gamePlayer.user_something = "Fill in Answer"
+                    print "response"
+                elif i==1: 
+                    self.gamePlayer.user_something = "Fill in Answer"
+                    print "response"
+                elif i==2: 
+                    self.gamePlayer.user_something = "Fill in Answer"
+                    print "response"
+                elif i==3: 
+                    self.gamePlayer.user_something = "Fill in Answer"
+                    print "response"
+                    
+                break
+  
+            else: 
+                print "Error"
+                self.getUserInput(acceptable_inputs)
